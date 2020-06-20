4aebf78dd463344cfaa9474e4f0ec418db66a942
Куликов Игорь Владиславович <Kulikov.I.Vla@sberbank.ru>
2020-06-21 00:22:24 +0300

    changes getting a path

--- lab_3/downloader.py
+++ lab_3/downloader.py
@@ -3,33 +3,35 @@ from os.path import isfile, join, basename
 import urllib.request
 from zipfile import ZipFile
 import shutil
+import os
 from distutils.dir_util import copy_tree
 
 pokemon_url = "https://github.com/kjaisingh/pokemon-classifier/archive/master.zip"
 pokemon_filename = "master.zip"
-pokemon_path = "data/train"
+pokemon_path = os.path.join("data", "train")
 temp_dir = "temp"
 
 class_names = ["bulbasaur", "charmander", "mewtwo", "pikachu", "squirtle"]
 
 def download_dataset(filename, url, work_dir):
-    if not path.exists(filename):
-        print("[INFO] Downloading pokemon dataset....")
-        filename, _ = urllib.request.urlretrieve(url, filename)
-        print("nice")
-        print("[INFO] Succesfully downloaded " + filename + " " + str(stat(filename).st_size) + " bytes.")
-        with ZipFile(filename, 'r') as archive:
-            for data in archive.namelist():
-                if data.endswith('.jpg'):
-                    print(work_dir)
-                    archive.extract(data, work_dir)
-            print("[INFO] Dataset extracted successfully.")
-            archive.close()
+    if path.exists(filename):
+        return
+    print("[INFO] Downloading pokemon dataset....")
+    filename, _ = urllib.request.urlretrieve(url, filename)
+    print("nice")
+    print("[INFO] Succesfully downloaded " + filename + " " + str(stat(filename).st_size) + " bytes.")
+    with ZipFile(filename, 'r') as archive:
+        for data in archive.namelist():
+            if data.endswith('.jpg'):
+                print(work_dir)
+                archive.extract(data, work_dir)
+        print("[INFO] Dataset extracted successfully.")
+        archive.close()
 
-        dst_dir = getcwd() + "/" + pokemon_path
-        src_dir = getcwd() + "/temp/pokemon-classifier-master/dataset/"
+    dst_dir = os.path.join(getcwd(), pokemon_path)
+    src_dir = os.path.join(getcwd(), temp_dir, "pokemon-classifier-master", "dataset")
 
-        copy_tree(src_dir, dst_dir)
+    copy_tree(src_dir, dst_dir)
 
 if __name__ == '__main__':
     if not path.exists(temp_dir):
