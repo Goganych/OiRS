2615f82e161002f01169d797cae1f287895816ad
Куликов Игорь Владиславович <Kulikov.I.Vla@sberbank.ru>
2020-06-22 21:57:43 +0300

    refactor code

--- lab_6/model.py
+++ lab_6/model.py
@@ -3,34 +3,44 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Reshape
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
-
+from tensorflow.keras.layers import Conv2D, LeakyReLU, BatchNormalization, Conv2DTranspose, Activation
+from tensorflow.keras import backend as K
+import numpy as np
 
 class Autoencoder:
 	# Размерность кодированного представления
-	encoding_dim = 128
+	encoding_dim = 256
 
 	def build(self, height, width, channels):
 		# Энкодер
 		# вход encoder
+		filters=(128, 64)
 		input_img = Input(shape=(height, width, channels))
-
+		x=input_img
 		# "вытянуть" картинку в одномерный вектор
-		flat_img = Flatten()(input_img)
+		for f in filters:
+			# apply a CONV => RELU => BN operation
+			x = Conv2D(f, (3, 3), strides=2, padding="same")(x)
+			x = LeakyReLU(alpha=0.2)(x)
+			x = BatchNormalization(axis=-1)(x)
+		volumeSize = K.int_shape(x)
+		flat_img = Flatten()(x)
 		# Кодированное полносвязным слоем представление
-		x = Dense(self.encoding_dim*4, activation='relu')(flat_img)
-		x = Dense(self.encoding_dim*3, activation='relu')(x)
-		x = Dense(self.encoding_dim*2, activation='relu')(x)
-		encoded = Dense(self.encoding_dim, activation='linear')(x)
+		encoded = Dense(self.encoding_dim, activation='relu')(flat_img)
 		
 		# Декодер
 		# Раскодированное другим полносвязным слоем изображение
-		input_encoded = Input(shape=(self.encoding_dim,))
-		x = Dense(self.encoding_dim*2, activation='relu')(input_encoded)
-		x = Dense(self.encoding_dim*3, activation='relu')(x)
-		x = Dense(self.encoding_dim*4, activation='relu')(x)
-		flat_decoded = Dense(height*width*channels, activation='linear')(x)
-
-		decoded = Reshape((height, width, channels))(flat_decoded)
+		input_encoded = Input(shape=(self.encoding_dim))
+		x = Dense(np.prod(volumeSize[1:]))(input_encoded)
+		x = Reshape((volumeSize[1], volumeSize[2], volumeSize[3]))(x)
+		for f in filters[::-1]:
+			# apply a CONV_TRANSPOSE => RELU => BN operation
+			x = Conv2DTranspose(f, (3, 3), strides=2,
+				padding="same")(x)
+			x = LeakyReLU(alpha=0.2)(x)
+			x = BatchNormalization(axis=-1)(x)
+		x = Conv2DTranspose(3, (3, 3), padding="same")(x)
+		decoded = Activation("sigmoid")(x)
 
 		# Модели, в конструктор первым аргументом передаются входные слои, а вторым выходные слои
 		# Другие модели можно так же использовать как и слои
